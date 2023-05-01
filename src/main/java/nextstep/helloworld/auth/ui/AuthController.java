b89271d5295ec1b9bbe4cf2c906d7cc0e373c677
odo27 <odo27@naver.com>
2023-05-01 14:27:22 +0900

    feat: sessionLogin 구현

--- src/main/java/nextstep/helloworld/auth/ui/AuthController.java
+++ src/main/java/nextstep/helloworld/auth/ui/AuthController.java
@@ -9,15 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Map;
 
 @RestController
 public class AuthController {
     private static final String SESSION_KEY = "USER";
+    private static final String USERNAME_FIELD = "email";
+    private static final String PASSWORD_FIELD = "password";
     private AuthService authService;
 
 
@@ -35,17 +40,16 @@ public class AuthController {
      * email=email@email.com&password=1234
      */
     @PostMapping("/login/session")
-    public ResponseEntity sessionLogin() {
-        // TODO: email과 password 값 추출하기
-        String email = "";
-        String password = "";
+    public ResponseEntity sessionLogin(HttpServletRequest request, HttpSession session) {
+        Map<String, String[]> paramMap = request.getParameterMap();
+        String email = paramMap.get(USERNAME_FIELD)[0];
+        String password = paramMap.get(PASSWORD_FIELD)[0];
 
         if (authService.checkInvalidLogin(email, password)) {
             throw new AuthorizationException();
         }
 
-        // TODO: Session에 인증 정보 저장 (key: SESSION_KEY, value: email값)
-
+        session.setAttribute(SESSION_KEY, email);
         return ResponseEntity.ok().build();
     }
 
@@ -57,9 +61,8 @@ public class AuthController {
      * accept: application/json
      */
     @GetMapping("/members/me")
-    public ResponseEntity findMyInfo() {
-        // TODO: Session을 통해 인증 정보 조회하기 (key: SESSION_KEY)
-        String email = "";
+    public ResponseEntity findMyInfo(HttpSession session) {
+        String email = (String) session.getAttribute(SESSION_KEY);
         MemberResponse member = authService.findMember(email);
         return ResponseEntity.ok().body(member);
     }
